22965fdddaf6e1a8d34ff56a8dadf5447a393e50
Swati Singhi <singhiswati11@gmail.com>
2016-04-16 00:19:54 +0530

    Setup the database interaction on local host as well as openshift.

--- application/controllers/Landing_page.php
+++ application/controllers/Landing_page.php
@@ -4,8 +4,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Landing_page extends CI_Controller {
 public function index()
 	{
-		$this->load->view('landing_page');
-		// $this->load->view('signin');
+		// $this->load->view('landing_page');
+		// $dsn ='mysqli://root:@localhost/legistifyphp';
+		//  $dbconnect = $this->load->database($dsn);
+		 $dbconnect = $this->load->database();
+
+
+	    /* Load the database model:
+	      /application/models/simple_model.php */
+	    $this->load->model('Simple_model');
+	    
+
+	    /* Create a table if it doesn't exist already */
+	    $this->Simple_model->create_table();
+	    
+	    
+	    /* Call the "insert_item" entry */
+	    $this->Simple_model->insert_item('Hello from Runnable!');
+
+	    /* Retrieve the last item  */
+	    print '<pre>';
+	    print_r($this->Simple_model->get_last_item());
+	    print '</pre>';
+
+	    /* Retrieve and print the row count */
+	    $rowcount = $this->Simple_model->get_row_count();
+	    print '<strong>Row count: ' . $rowcount . '</strong>';
+    
 	}
 public function signin()
 	{
